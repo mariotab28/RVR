81ac197a085d85c772c709f64b5d418945f318df
argarc03 <argarc03@ucm.es>
2020-05-14 11:15:35 +0200

    Empezada práctica 2.2

---
+++ practica2.2/serializacion/ejercicio1.cc
@@ -0,0 +1,51 @@
+#include "Serializable.h"
+
+#include <iostream>
+#include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+class Jugador: public Serializable
+{
+public:
+    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    {
+        strncpy(name, _n, 80);
+    };
+
+    virtual ~Jugador(){};
+
+    void to_bin()
+    {
+        //
+    }
+
+    int from_bin(char * data)
+    {
+        //
+        return 0;
+    }
+
+
+public:
+    char name[80];
+
+    int16_t x;
+    int16_t y;
+};
+
+int main(int argc, char **argv)
+{
+    Jugador one_r("", 0, 0);
+    Jugador one_w("Player_ONE", 123, 987);
+
+    //Serializar y escribir one_w en un fichero
+    //Leer el fichero en un buffer y "deserializar" en one_r
+    //Mostrar el contenido one_r
+
+    return 0;
+}
